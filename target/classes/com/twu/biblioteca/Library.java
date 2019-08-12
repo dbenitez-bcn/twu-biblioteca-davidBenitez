a66c18161e74804e4fa63145fbf08f228d4694b9
David Benitez Riba <david@MacBook-Pro-de-David.local>
2019-08-12 17:44:58 +0200

    Intellij stuff

---
+++ target/classes/com/twu/biblioteca/Library.java
@@ -0,0 +1,53 @@
+package com.twu.biblioteca;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Library {
+    private ArrayList<Book> collection;
+
+    public Library() {
+
+    }
+
+    public Library(ArrayList<Book> collection) {
+        this.collection = collection;
+    }
+
+    public static Library emptyCollection() {
+        ArrayList<Book> books = new ArrayList<Book>();
+        return new Library(books);
+    }
+
+    public static Library filledCollection() {
+        ArrayList<Book> books = new ArrayList<Book>(
+                Arrays.asList(
+                        new Book("Brandom Sanderson", 2009),
+                        new Book("Patrick Rothfuss", 1999),
+                        new Book("George R.R. Martin", 2019),
+                        new Book("Anonymous", 2003),
+                        new Book("J.K. Rowling", 2009),
+                        new Book("J.R.R. Tolkien", 2009),
+                        new Book("Agatha Christie", 2009),
+                        new Book("Laura Gallego", 2009),
+                        new Book("Stephen King", 2009),
+                        new Book("Christie Golden", 2009)
+                )
+        );
+
+        return new Library(books);
+    }
+
+    public static Library fromArray(Book[] list) {
+        ArrayList<Book> books = new ArrayList<Book>(
+                Arrays.asList(list)
+        );
+
+        return new Library(books);
+    }
+
+    public int countOfBooks() {
+        return this.collection.size();
+    }
+}
---
+++ target/test-classes/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,61 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+public class LibraryTest {
+    @Test
+    public void libraryExists() {
+        Library actual;
+
+        actual = new Library();
+
+        assertThat(actual, notNullValue());
+    }
+
+    @Test
+    public void libraryCreatedWithZeroBooks() {
+        Library library;
+        int actual;
+        int expected;
+
+        library = Library.emptyCollection();
+        actual = library.countOfBooks();
+        expected = 0;
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void libraryCreatedWithTenBooks() {
+        Library library;
+        int actual;
+        int expected;
+
+        library = Library.filledCollection();
+        actual = library.countOfBooks();
+        expected = 10;
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void libraryShouldBeCreatedFromAnArray() {
+        Library library;
+        Book[] books;
+        Book bookTemplate;
+        int actual;
+        int expected;
+
+        bookTemplate = new Book("Author testerino", 9999);
+        books = new Book[]{bookTemplate, bookTemplate, bookTemplate, bookTemplate, bookTemplate};
+        library = Library.fromArray(books);
+        actual = library.countOfBooks();
+        expected = 5;
+
+        assertThat(actual, is(expected));
+    }
+}
