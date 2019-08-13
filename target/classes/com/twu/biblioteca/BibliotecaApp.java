6a94d71ebbed30b6bf9ca4968f5606478d5ffa78
David Benitez Riba <david@MacBook-Pro-de-David.local>
2019-08-13 14:28:01 +0200

    command line interface added

--- target/classes/com/twu/biblioteca/BibliotecaApp.java
+++ target/classes/com/twu/biblioteca/BibliotecaApp.java
@@ -3,24 +3,25 @@ package com.twu.biblioteca;
 import java.util.Scanner;
 
 public class BibliotecaApp {
+    private static Library library = Library.filledCollection();
 
     public static void main(String[] args) {
         showWelcomeMessage();
         showMenu();
     }
 
-    private static void showWelcomeMessage(){
+    private static void showWelcomeMessage() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great books titles in Bangalore!");
     }
 
-    private static void showMenu(){
+    private static void showMenu() {
         boolean exit = false;
-        while(!exit){
+        while (!exit) {
             int optionSelected;
 
             System.out.println("===1.List of books | 2.Quit===");
-            optionSelected = getMenuOption();
-            switch (optionSelected){
+            optionSelected = getUserInputAsNumber();
+            switch (optionSelected) {
                 case 1:
                     showList();
                     break;
@@ -28,36 +29,68 @@ public class BibliotecaApp {
                     exit = true;
                     break;
                 default:
-                    System.out.println("Please select a valid option!");
+                    showInvalidOptionMessage();
+                    break;
             }
         }
     }
 
-    private static int getMenuOption(){
+    private static int getUserInputAsNumber() {
         String userInput = getUserInput();
-        try{
+        try {
             return Integer.parseInt(userInput);
-        }catch (Exception e){
+        } catch (Exception e) {
         }
         return -1;
     }
 
-    private static String getUserInput(){
+    private static String getUserInput() {
         Scanner s = new Scanner(System.in);
         return s.nextLine();
     }
 
-    private static void showList(){
-        Library library = Library.filledCollection();
+    private static void showList() {
         boolean exit = false;
-        while (!exit){
+        while (!exit) {
             int optionSelected;
 
             library.showList();
             System.out.println("\n===1.Checkout a book | 2.Return a book | 3.Back===");
-            optionSelected = getMenuOption();
-
-
+            optionSelected = getUserInputAsNumber();
+            switch (optionSelected) {
+                case 1:
+                    checkoutABook();
+                    break;
+                case 2:
+                    checkinABook();
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default:
+                    showInvalidOptionMessage();
+                    break;
+            }
         }
+    }
+
+    private static void checkoutABook() {
+        int bookNumber;
+
+        System.out.println("Type the number of the book to checkout");
+        bookNumber = getUserInputAsNumber();
+        library.checkOut(bookNumber);
+    }
+
+    private static void checkinABook() {
+        int bookNumber;
+
+        System.out.println("Type the number of the book to checkin");
+        bookNumber = getUserInputAsNumber();
+        library.checkIn(bookNumber);
+    }
+
+    private static void showInvalidOptionMessage() {
+        System.out.println("Please select a valid option!");
     }
 }
